71fbbaab9a823c822a98e211695c5dc0228ae68d
Pioneer <80742588+Wenutu@users.noreply.github.com>
2023-02-21 13:43:47 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,4 @@
 module.exports = {
     // 禁止hash name
     filenameHashing:false,
-    publicPath:'/',
-    devServer:{
-        historyApiFallback:{
-            index:'/index.html'
-        }
-    }
 }
