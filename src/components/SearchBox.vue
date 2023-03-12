bae3aab2321539f7281b47a6a5a8d642ec707e91
Pioneer <80742588+Wenutu@users.noreply.github.com>
2023-03-12 20:26:31 +0800

    version 2.2

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -13,7 +13,7 @@
       <div>
         <div>
             <form :action="searchUrl" method="GET" target="_blank" class="width=100%">
-              <input type="text" name="q" v-model="searchText" :placeholder="searchPlaceholder" />
+              <input type="text" :name="searchName" v-model="searchText" :placeholder="searchPlaceholder" />
               <button type="submit"><i class="fa fa-search"></i></button>
             </form>
           </div>
@@ -39,19 +39,19 @@
       return {
         categories: [
         { id: 1, name: '常用', searchEngines: [
-          { id: 11, name: 'Google', url: 'https://www.google.com/search?q=',placeholder:'google 一下' },
-          { id: 12, name: 'Bing', url: 'https://www.bing.com/search?q=',placeholder:'bing 两下' },
-          { id: 13, name: '百度',url:'https://www.baidu.com/s?wd=',placeholder:'无奈之举'}
+          { id: 11, name: 'Google', url: 'https://www.google.com/search',placeholder:'google 一下',n:'q'},
+          { id: 12, name: 'Bing', url: 'https://www.bing.com/search',placeholder:'bing 两下',n:'q' },
+          { id: 13, name: '百度',url:'https://www.baidu.com/s',placeholder:'无奈之举',n:'wd'}
         ] },
         { id: 2, name: '社区', searchEngines: [
-          { id: 21, name: 'Github', url: 'https://github.com/search?q=',placeholder:'Github' },
-          { id: 22, name: 'Stackoverflow', url: 'https://stackoverflow.com/search?q=',placeholder:'Stackoverflow' },
-          { id: 23, name: 'Gitee',url: 'https://search.gitee.com/?skin=rec&type=repository&q=',placeholder:'码云'}
+          { id: 21, name: 'Github', url: 'https://github.com/search',placeholder:'Github',n:'q' },
+          { id: 22, name: 'Stackoverflow', url: 'https://stackoverflow.com/search',placeholder:'Stackoverflow' ,n:'q'},
+          { id: 23, name: 'Gitee',url: 'https://search.gitee.com/?skin=rec&type=repository',placeholder:'码云',n:'q'}
         ] },
         { id: 3, name: '生活', searchEngines: [
-          { id: 31, name: '知乎', url: 'https://www.zhihu.com/search?type=content&q=',placeholder:'知乎' },
-          { id: 32, name: '微博', url: 'https://s.weibo.com/weibo?q=',placeholder:'微博' },
-          { id: 33, name: '哔哩哔哩',url:'https://search.bilibili.com/all?keyword=',placeholder:'B站'}
+          { id: 31, name: '知乎', url: 'https://www.zhihu.com/search?type=content',placeholder:'知乎',n:'q' },
+          { id: 32, name: '微博', url: 'https://s.weibo.com/weibo',placeholder:'微博' ,n:'q'},
+          { id: 33, name: '哔哩哔哩',url:'https://search.bilibili.com/all',placeholder:'B站',n:'keyword'}
         ] }
       ],
         activeCategory: 1,
@@ -67,11 +67,19 @@
         const replace =this.activeSearchEngines.find(se => se.id === this.activeSearchEngine)
         return replace.placeholder
       },
+      searchName(){
+        if (this.activeCategory && this.activeSearchEngine) {
+          const searchEngine = this.activeSearchEngines.find(se => se.id === this.activeSearchEngine)
+          //const query = encodeURIComponent(this.searchText) 
+          return `${searchEngine.n}`
+        }
+        return 'q'
+      },
       searchUrl() {
         if (this.activeCategory && this.activeSearchEngine) {
           const searchEngine = this.activeSearchEngines.find(se => se.id === this.activeSearchEngine)
-          const query = encodeURIComponent(this.searchText) 
-          return `${searchEngine.url}${query}`
+          //const query = encodeURIComponent(this.searchText) 
+          return `${searchEngine.url}`
         }
         return ''
       }
